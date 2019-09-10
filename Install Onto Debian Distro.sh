09e7417ffd1d89f66b56a263ffd2719d69066bf4
omni-custom <55126115+omni-custom@users.noreply.github.com>
2019-09-10 02:55:32 -0400

    Update Install Onto Debian Distro.sh

--- Install Onto Debian Distro.sh
+++ Install Onto Debian Distro.sh
@@ -6,4 +6,4 @@ USER_NAME=$(dialog --inputbox "Tell me, what is your name? I'm only asking to im
 USER_COLOR=$(dialog --menu "Favorite color?" 0 0 0 red Red orange Orange yellow Yellow lime Lime green Green teal Teal cyan Cyan lightBlue "Light Blue" blue Blue indigo Indigo violet Violet magenta Magenta pink Pink white White lightGray "Light Gray" gray Gray black Black)
 USER_SECOND_COLOR=$(dialog --menu "2nd favorite color?" 0 0 0 red Red orange Orange yellow Yellow lime Lime green Green teal Teal cyan Cyan lightBlue "Light Blue" blue Blue indigo Indigo violet Violet magenta Magenta pink Pink white White lightGray "Light Gray" gray Gray black Black)
 USER_TASKBAR_POS=$(dialog --menu "Preferred taskbar position?" 0 0 0 top Top bottom "Bottom (Default)" left Left right Right)
-echo USER_NAME=$USER_NAME\nUSER_COLOR=$USER_COLOR\nUSER_SECOND_COLOR=$USER_SECOND_COLOR\n
+echo USER_NAME=$USER_NAME\nUSER_COLOR=$USER_COLOR\nUSER_SECOND_COLOR=$USER_SECOND_COLOR\nUSER_TASKBAR_POS=$USER_TASKBAR_POS>/var/theme/vars.sh
