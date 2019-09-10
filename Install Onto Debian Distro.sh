3ab1ed2c9509adab74773a61d252095854109d54
omni-custom <55126115+omni-custom@users.noreply.github.com>
2019-09-10 02:56:27 -0400

    Update Install Onto Debian Distro.sh

--- Install Onto Debian Distro.sh
+++ Install Onto Debian Distro.sh
@@ -1,6 +1,6 @@
 echo Hold on...
 echo You're going to need sudo privileges, so make sure you're running under sudo.
-apt-get install dialog
+apt-get install dialog awesome lightdm -y
 dialog --msgbox "Hello there!"
 USER_NAME=$(dialog --inputbox "Tell me, what is your name? I'm only asking to improve your experience.")
 USER_COLOR=$(dialog --menu "Favorite color?" 0 0 0 red Red orange Orange yellow Yellow lime Lime green Green teal Teal cyan Cyan lightBlue "Light Blue" blue Blue indigo Indigo violet Violet magenta Magenta pink Pink white White lightGray "Light Gray" gray Gray black Black)
